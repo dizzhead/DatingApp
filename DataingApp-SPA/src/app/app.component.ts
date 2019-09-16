f8827635235c431a18bb380848d2ceda07dd5f71
Caprathe <Joseph.Caprathe@ascension.org>
2019-09-16 11:09:34 -0400

    end of section 4

--- DataingApp-SPA/src/app/app.component.ts
+++ DataingApp-SPA/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'The Application';
+  title = 'The Dating Application';
 }
